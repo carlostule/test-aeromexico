3c3ffae01e02002603bf7db5527430a55e46a27f
Carlos Enrique Tule Uscanga <carlosenrique.tule.contractor@bbva.com>
2022-09-12 01:34:54 -0500

    Updates

--- src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { characterReducers } from "./characterReducers";
-
-const reducers = combineReducers({
-  allCharacters: characterReducers,
-});
-
-export default reducers;
--- src/utils/getWindowSize.js
+++
@@ -1,4 +0,0 @@
-export function getWindowSize() {
-  const { innerWidth } = window;
-  return innerWidth;
-}
